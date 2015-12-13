c641780af549bab554760f634a72696ba0dd78ee
cian <cian@ciangallagher.net>
2015-12-13 22:11:39 +0000

    Cleaned up code and created partials. Applying the DRY philosophy.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
   # Validate specific fields passed to the article model
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :author, :title, :text, presence: true,
             length: { minimum: 5 }
 end
