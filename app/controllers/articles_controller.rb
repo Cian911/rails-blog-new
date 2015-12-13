6ec3aad942f97017735f6d3001d3c6a8d767e16b
cian <cian@ciangallagher.net>
2015-12-13 22:21:16 +0000

    Added very basic user authenitcation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 
+  # Basic User authentication system, allows access to a requested
+  # Method only if the action allows it.
+  http_basic_authenticate_with name: "Cian", password: "password", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,9 @@
 class CommentsController < ApplicationController
 
+  # We obviously dont want any user to be able to delete comments
+  # So add an authentication layer here too.
+  http_basic_authenticate_with name: "Cian", password: "password", only: :destroy
+
   def create
     @article = Article.find(params[:article_id])
     @comments = @article.comments.create(comment_params)
